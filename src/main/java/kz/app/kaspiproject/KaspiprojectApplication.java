2451b1f516dbc7003b6d57af4a2dc769a6b95f4f
k_zhazykbai <kuanysh020402@gmail.com>
2026-02-02 22:59:16 +0500

    start project

---
+++ src/main/java/kz/app/kaspiproject/KaspiprojectApplication.java
@@ -0,0 +1,13 @@
+package kz.app.kaspiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaspiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaspiprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/app/kaspiproject/KaspiprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.app.kaspiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaspiprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
